548a48a7c7320ccde7f1c259247b27c9296a5106
Marian <115044973+mgorno@users.noreply.github.com>
2025-07-06 15:00:08 -0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -9,10 +9,9 @@ def register_all_blueprints(app):
     from .pedidos import bp_pedidos
     from .productos import bp_productos
 
-    app.register_blueprint(bp_clientes)
-    app.register_blueprint(bp_main)
-    app.register_blueprint(bp_pagos)
-    app.register_blueprint(bp_pedidos)
-    app.register_blueprint(bp_productos)
-    app.register_blueprint(bp)
-    
+    app.register_blueprint(bp_clientes, url_prefix='/clientes')
+    app.register_blueprint(bp_main)  # para la raíz /
+    app.register_blueprint(bp_pagos, url_prefix='/pagos')
+    app.register_blueprint(bp_pedidos, url_prefix='/pedidos')
+    app.register_blueprint(bp_productos, url_prefix='/productos')
+    app.register_blueprint(bp)  # tu blueprint raíz, si tiene rutas

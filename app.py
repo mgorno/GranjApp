1c823b9d738302f61b75bfe77561b303b4e53c9a
Marian <115044973+mgorno@users.noreply.github.com>
2025-07-07 23:33:10 -0300

    subo cambios pequños par qeu no explote todo

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ import os
 from flask import Flask
 from models import init_db
 from routes import register_all_blueprints
-from models import init_db 
+
 app = Flask(__name__)
 init_db()
 app.secret_key = os.environ.get("SECRET_KEY", "dev-key")
-register_all_blueprints(app) 
+register_all_blueprints(app)
+
+def formato_cantidad(n):
+    try:
+        n = float(n)
+        return str(int(n)) if n == int(n) else f"{n:.3f}".rstrip("0").replace(".", ",")
+    except (ValueError, TypeError):
+        return "0"
+
+def formato_precio(n):
+    try:
+        n = float(n)
+        return f"${int(n)}" if n == int(n) else f"${n:.2f}".rstrip("0").rstrip(".")
+    except (ValueError, TypeError):
+        return "$0"
+
+app.jinja_env.filters["formato_cantidad"] = formato_cantidad
+app.jinja_env.filters["formato_precio"] = formato_precio
 
 if __name__ == "__main__":
     port  = int(os.environ.get("PORT", 5000))

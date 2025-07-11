2bea65f68320d36d556d43d450810a27ee32b9cb
Marian <115044973+mgorno@users.noreply.github.com>
2025-07-11 13:07:08 -0300

    agrego cuenta corriente

--- routes/__init__.py
+++ routes/__init__.py
@@ -9,6 +9,7 @@ def register_all_blueprints(app):
     from .pedidos import bp_pedidos
     from .productos import bp_productos
     from .entregas import bp_entregas  
+    from .entregas import bp_cuenta_corriente
 
     app.register_blueprint(bp_clientes, url_prefix='/clientes')
     app.register_blueprint(bp_main)
@@ -16,4 +17,5 @@ def register_all_blueprints(app):
     app.register_blueprint(bp_pedidos, url_prefix='/pedidos')
     app.register_blueprint(bp_productos, url_prefix='/productos')
     app.register_blueprint(bp_entregas, url_prefix='/entregas') 
+    app.register_blueprint(bp_cuenta_corriente, url_prefix='/cuenta_corriente') 
     app.register_blueprint(bp)

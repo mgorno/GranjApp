091bc2cd7dbae7f86816e430ff18bb431054862a
Marian <115044973+mgorno@users.noreply.github.com>
2025-07-14 19:22:56 -0300

    corrijo blueprints

--- routes/remitos_generados.py
+++ routes/remitos_generados.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models import get_conn
 
-bp_remitos = Blueprint("remitos_generados", __name__, url_prefix="/remitos-generados")
+bp_remitos = Blueprint("remitos_generados", __name__, url_prefix="/remitos_generados")
 
 @bp_remitos.route("/", methods=["GET"])
 def lista_remitos():

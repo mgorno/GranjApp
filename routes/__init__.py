1c234eebd89cecf4b8b700fb7429198c14111b30
Marian <115044973+mgorno@users.noreply.github.com>
2025-07-15 18:27:54 -0300

    sacco main duplicado de bp

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 bp = Blueprint('root', __name__)
 def register_all_blueprints(app):
     from .clientes import bp_clientes
-    from .main import bp_main
     from .pagos import bp_pagos
     from .pedidos import bp_pedidos
     from .productos import bp_productos
@@ -14,7 +13,6 @@ def register_all_blueprints(app):
 
 
     app.register_blueprint(bp_clientes, url_prefix="/clientes")
-    app.register_blueprint(bp_main)
     app.register_blueprint(bp_pagos)
     app.register_blueprint(bp_pedidos)
     app.register_blueprint(bp_productos)
